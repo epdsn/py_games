0c3308364cd45538fb1289f099ddaa871794446d
Eric Perez <ibestudios@gmail.com>
2025-10-08 07:12:21 -0700

    Correct syntax and operator logic in calculator
    Fix various syntax errors and improve operator handling in calculator.py.

--- calculator.py
+++ calculator.py
@@ -1,29 +1,4 @@
 
-left - int(dpg-get_value("left_operand" ))
-
-right - int(dpg-get_value("right_operand" ))
-1f dps-get_value("operator") -=
-"+":
-
-dpg-set_ value( str (left + right)
-
-if dpg-get_value("operator") -
-dpg-set_value str (left - right) )
-
-if dpg-get_value("operator") -= */":
-dpg-set_value( str (left / right) )
-
-if dpg-get_value("operator")
--- *x*:
-dpg-set_value( str (left * right) )
-
-if dpg-get_value("operator")
-*%*:
-dpg-set_value( str (left % right) )
-mainpy
-alc apppy 1 x
-A › Cosches > Coach Eric >
-* calc. app-py > 6 update operator
 Import dearpygul,dearpyaul, as dpe
 def update number (sender, app_data, user_data) :
 print(user data)
@@ -55,23 +30,13 @@ def perforn_operation(sender, app_data):
 dpg-create_context()
 dpg-create_vicuport()
 dpg-setup dearpygui ()
-39
-41
-4고
-43
-44
-45
+
+
 with dpg-texture_registry():
 width, height, channel, data, - dpg. load _image("delete_button-png")
 dpg-add _static_texture(width-width, height-height, default_valus-data, tag-"delete_button_texture*)
-47
-4룸
-49
-58
-51
-52
-53
-54
+
+
 with dpg-value_registry():
 dpg-add _string_value(default_valuc="", tag-"left_operand" )
 dpg-add_string_value(default_value-"*,
@@ -81,25 +46,16 @@ with dpg-window(label-"Example Window")as main_window:
 with dps-group(horizontal-Truc) :
 dpg-add_ text(source-"left_operand" )
 dpg-add _text (source-"operator") dpg-add_text(source-"right_operand")
-56
-57
-58
-59
-68）
-61
-62
+
+
 with dpg-group(horizontal-Truc):
 dpg-add_button(label-"7", callback-update_number, user_data-"7",
 width-108, height-100)
 dpg-add_button(label-"g", callback-update_number, user_data-"8", width-188, height-100) dpg-add_button(label-"9", callback-update_number, user_data-"9",
 width-100, height-100)
 dpg-add image_button(texture_tag-"delete_button_texture", callback-delete_number, tag-"delete", user_data-"delete_number", width-58, height-50) dpg-add_button(label-"/", callback-update_operator, user_data-"/*, width-180, height-100)
-64
-65
-66
-67
-68
-69
+
+
 with dpg-group(horizonta)-True): dpg-add_button(label-"*"
 callback-update_number,user_data-"4",
 dpg-add_ button(label-"5,
@@ -108,19 +64,8 @@ width-100, height-100) width-100, height-100)
 dpg-add_button(label-"6",
 callback-update_number, user _data-*6", width-100, height-100)
 dpg-add_ button(label-"x", callback-update_operator,user _data-"x", width-100, height-100)
-71
-72
-73
-74
-75
-76
-77
-78
-79
-8e
-81
-82
-Nth
+
+
 dps-group(horizontal-True):
 dE-add_button(label-"3", callback-update_number, user_data dpg-add_button(label-"2",
 callback-update_number, user_data»
@@ -139,9 +84,8 @@ callback-update_operator, user_data
 dpg-add_button(label-"**, callback-update_ operator, user.
 ,width-100, height-100)
 width-180, height-188) wddth-100, height-200)
-85
-86
-ВОДІ
+
+
 with dipe thene() as global theme:
 with dp-thems component (4pg-mvButton):
 epp-add there_style(dpg-myStyleVar Franckounding,50, category-dpg-avTheneCat_Care)
